3919beb18555b539906295c3a08ab384122b8b9c
Ravneet Anand <anand.ravneet099@gmail.com>
2024-02-25 22:53:08 +0000

    Remove unused file.

--- src/utils/queries/index.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-module.exports.get_players = `
-    SELECT id, Location location, Title_DistinctivetitlebookCovertitle_TitleText title, Contributor1_PersonName contributor, Cover_File coverfile, Unique_URL uniqueUrl
-    FROM dbo.assessment2;`;
